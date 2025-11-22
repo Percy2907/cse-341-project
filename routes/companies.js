616527e328d83a6d67edab88345a8e238a76585b
Percy2907 <pyarlequejara@byupathway.edu>
2025-11-22 01:07:22 -0500

    W04 Learning Activity: OAuth

--- routes/companies.js
+++ routes/companies.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const companiesController = require("../controllers/companies");
+const {isAuthenticated} = require("../middleware/authenticate")
 
 router.get("/", companiesController.getAll);
 router.get("/:id", companiesController.getSingle);
-router.post("/", companiesController.createCompany);
-router.put("/:id", companiesController.updateCompany);
-router.delete("/:id", companiesController.deleteCompany);
+router.post("/", isAuthenticated, companiesController.createCompany);
+router.put("/:id", isAuthenticated, companiesController.updateCompany);
+router.delete("/:id", isAuthenticated, companiesController.deleteCompany);
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,20 @@
 const express = require("express");
+const passport = require("passport");
 const router = express.Router();
 
 router.use("/contacts", require("./contacts"));
 router.use("/companies", require("./companies"));
-router.use("/", require("./swagger"));
+router.use("/", require("./swagger")); 
+router.get("/login", passport.authenticate("github"));
+
+router.get('/logout', function(req, res, next) {
+
+  req.logout(function(err) {
+    if (err) { return next(err); }
+    req.session.destroy((err) => {
+      res.redirect("/");
+    });
+  });
+});
 
 module.exports = router;
